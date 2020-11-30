64c25a9f2df7dd11bd3e719b8ba428434d8047f4
Mohineesh Chennamsetti <mohineesh.wcch@gmail.com>
2020-11-30 15:09:28 +0530

    Edit update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,16 @@ class UsersController<ApplicationController
     def show
         @user=User.find(params[:id])
     end    
+    def edit
+        @user=User.find(params[:id])
+    end
+    def update
+        @user=User.find(params[:id])
+        if @user.update(params.require(:user).permit(:username,:email,:password))
+            flash[:notice]="USer updated #{@user.username}"
+            redirect_to articles_path
+        else
+            render 'edit' 
+        end       
+    end     
 end
